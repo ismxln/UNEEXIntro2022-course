e7751c38d507c7532ffcb14a6306c4689c3b97d0
Maxelone <maxelone@yandex.ru>
2023-02-05 03:34:23 +0400

    Added lesson 2

---
+++ lesson2.py
@@ -0,0 +1,11 @@
+while str := input(':= '):  # while input is string then print
+    print()
+  # same as
+a = input('= ')
+while a:
+    print(a)
+    a = input()
+
+
+print(1,2,3, sep='QQQ')
+print(1,2,3, sep='---')
